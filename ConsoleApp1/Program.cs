324d47f94747ce614ffb57701afdb9b1ba9096e8
Peter Ibbotson <peter@ibbotson.co.uk>
2021-02-28 18:27:34 +0000

    Made cecil read into memory.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var assembly = AssemblyDefinition.ReadAssembly("C:\\Users\\peter\\source\\repos\\peteri\\AttributeRemover\\AttributeRemover.TestAssembly\\bin\\Debug\\publish\\netcoreapp3.1\\AttributeRemover.TestAssembly.dll");
+            var assembly = AssemblyDefinition.ReadAssembly(
+                "C:\\Users\\peter\\source\\repos\\peteri\\AttributeRemover\\AttributeRemover.TestAssembly\\bin\\Debug\\publish\\netcoreapp3.1\\AttributeRemover.TestAssembly.dll",
+                new ReaderParameters { ReadingMode = ReadingMode.Immediate, ReadWrite = true, InMemory = true });
             var attributesToRemove = assembly.CustomAttributes
                 .Where(ca => ca.AttributeType.FullName == "System.Runtime.CompilerServices.InternalsVisibleToAttribute")
                 .ToArray();
